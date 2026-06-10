12db6b2581beca8cd56e5345c78f98427a42032c
blueRay <delapena.giselle1@gmail.com>
2026-06-10 16:40:20 +0800

    feat: add auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
+const authMiddleWare = require('./middleware/auth')
 
 const app = express()
 
@@ -14,6 +15,8 @@ app.get('/health', (req, res) => {
     res.json({ status: 'ok'})
 })
 
-
+app.get('/protected', authMiddleWare, (req, res) => {
+    res.json({ message: "You are now logged in", user: req.user})
+})
 
 module.exports = app
