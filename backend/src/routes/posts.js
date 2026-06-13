6dfd9158db6c546130e695a5c582a79193ae9ece
blueRay <delapena.giselle1@gmail.com>
2026-06-13 08:29:46 +0800

    feat: add post routes (CRUD + resolve)

---
+++ backend/src/routes/posts.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../config/db')
+const authMiddleWare = require('../middleware/auth')
+const { post } = require('../app')
+
+router.get('/', async(req,res) => {
+    
+    try {
+        const posts = await pool.query('SELECT * FROM posts')
+        res.status(200).json(posts.rows)
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Not found.'})
+    }
+
+})
+
+router.get('/:id', async(req, res) =>{
+    try {
+        const { id } = req.params
+        const post = await pool.query('SELECT * FROM posts WHERE id = $1', [id])
+
+        if (post.rows.length === 0) {
+            return res.status(404).json({ message: "No posts." });
+        } 
+        res.status(200).json(post.rows[0])
+    } catch (error) {
+        res.status(500).json({ message: "Server error." });
+    }
+})
+
+router.post('/', authMiddleWare, async(req, res) => {
+    try {
+        const id = req.user.id
+        const { itemName, category, building, room, handedToSecurity } = req.body;
+
+        const newPost = await pool.query('INSERT INTO posts (user_id, title, category, building, room, status, handed_to_security) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [id, itemName, category, building, room, 'Found', handedToSecurity])
+
+        res.status(200).json(newPost.rows[0])
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server error' })
+    }
+})
+
+router.patch('/:id/resolve', async(req, res) => {
+    try {
+        const id = req.params.id
+        const updated_post = await pool.query('UPDATE posts SET is_resolved = true WHERE id = $1 RETURNING *', [id])
+
+        if (updated_post.rows.length === 0) {
+            return res.status(404).json({ message: "Post not found."})
+        } else {
+            res.status(200).json(updated_post.rows[0])
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server error' })
+    } 
+})
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id 
+
+    try {
+        const deleted_posts = await pool.query('DELETE FROM posts WHERE id = $1 RETURNING *', [id]) 
+
+        if (deleted_posts.rows.length === 0) {
+            return res.status(404).json({ message : "Post not found."})
+        } else {
+            res.status(200).json({ message: "Post deleted successfully."})
+        }
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Server error."})
+    } 
+
+})
+
+module.exports = router
